9c332ac7937c68b8fcf8571a850182453c5bb3b4
PhoenixGS <thestarrydream@gmail.com>
2024-04-27 14:20:37 +0800

    update Exp2
    完成report，添加注释等

--- Exp2/k-means.py
+++ Exp2/k-means.py
@@ -1,53 +1,51 @@
 import torch
+from sklearn.manifold import TSNE
 import numpy as np
 import random
 from torchvision import datasets
+import matplotlib.pyplot as plt
 
-def load_data():
+def load_data(): # load MNIST data
     train_data = datasets.MNIST(root = './data', train=True, download=True)
     images = train_data.data.numpy()
     labels = train_data.targets.numpy()
     return images, labels
 
-def pr(image):
+def pr(image): # print image
     for i in range(len(image)):
         for j in range(len(image[i])):
             print('.' if image[i][j] < 128 else '#', end='')
         print()
 
 class k_means:
-    def __init__(self, data, label=None, k=10):
+    def __init__(self, data, k=10): # initialize k-means
         self.data = data
-        # self.label = label
         self.k = k
-        # self.centers = np.array([self.data[np.argwhere(label == i)[0][0]] for i in range(k)])
-        # self.centers = np.array([data[i] for i in random.sample(range(len(data)), k)])
         index = np.array(random.sample(range(len(data)), k))
         self.centers = data[index]
         self.label = np.zeros(len(data))
         self.update_label()
 
     def update_label(self):
-        for i in range(len(self.data)):
-            distances = [np.linalg.norm(self.data[i] - self.centers[c]) for c in range(self.k)]
+        count = 0
+        for i in range(len(self.data)): # update labels of each sample
+            distances = [np.linalg.norm(self.data[i] - self.centers[c], ord=np.inf) for c in range(self.k)]
             cluster = np.argmin(distances)
+            count += (self.label[i] != cluster)
             self.label[i] = cluster
+        return count <= len(self.data) // 1000 # return True if the number of samples whose labels are changed is less than 0.1% of the total number of samples
     
     def iterate(self):
-        for c in range(self.k):
-            # self.centers[c] = np.mean(self.data[np.argwhere(self.label == c)], axis=0)
-            if np.sum(self.label == c) == 0:
-                assert(False)
+        for c in range(self.k): # update centers of clusters
             index = np.argwhere(self.label == c)
             index = index.reshape(index.shape[0])
             center = np.mean(self.data[index], axis=0)
-            # self.centers[c] = self.data[np.argmin([np.linalg.norm(center - self.data[i]) for i in range(len(self.data))])]
             self.centers[c] = center
-        self.update_label()
+        return self.update_label() # update labels
 
     def max_distance(self):
         maxdistance = 0
-        for i in range(len(self.data)):
+        for i in range(len(self.data)): # calculate the max distance between each sample and its center
             distance = np.linalg.norm(self.data[i] - self.centers[int(self.label[i])])
             if distance > maxdistance:
                 maxdistance = distance
@@ -55,24 +53,44 @@ class k_means:
 
     def calc_label(self, labels):
         self.caption = np.zeros(self.k)
-        for k in range(self.k):
+        for k in range(self.k): # calculate the label of each cluster
             label = np.argmax([np.sum(labels[np.argwhere(self.label == k)] == i) for i in range(10)])
             self.caption[k] = label
-        return [self.caption[int(self.label[i])] for i in range(len(self.data))]
+        return [self.caption[int(self.label[i])] for i in range(len(self.data))] # return the label of each sample
 
 if __name__ == '__main__':
-    random.seed(0)
+    random.seed(1)
     images, labels = load_data()
-    images = np.array(images, dtype=np.float)
-    labels = np.array(labels, dtype=np.float)
-    kmeans = k_means(images, k=10)
+    images = np.array(images, dtype=np.float64)
+    labels = np.array(labels, dtype=np.float64)
+    kmeans = k_means(images, k=10) # initialize k-means
     
-    pre_maxd = np.inf
-    while kmeans.max_distance() > 1:
-        kmeans.iterate()
-        if kmeans.max_distance() >= pre_maxd:
+    while True:
+        if kmeans.iterate():
             break
-        pre_maxd = kmeans.max_distance()
+        print(kmeans.max_distance()) # output max distance each iteration
+        print("Accuracy:", np.sum(kmeans.calc_label(labels) == labels) / len(labels)) # output accuracy each iteration
 
-    print(kmeans.max_distance())
-    print("Accuracy:", np.sum(kmeans.calc_label(labels) == labels) / len(labels))
+
+    x_low = TSNE(n_components=3, random_state=0).fit_transform(images[:10000].reshape(len(images[:10000]), -1)) # t-SNE
+
+    # output wrong cases
+    # y = kmeans.calc_label(labels)
+    # wrong_idx = np.argwhere(y != labels)
+    # for i in wrong_idx[:10]:
+    #     print(y[i[0]], labels[i[0]])
+    #     pr(images[i[0]])
+    #     print()
+
+    # unique y
+    print(np.unique(y, return_inverse=True)[0])
+
+    # 2-d plot
+    # plt.scatter(x_low[:, 0], x_low[:, 1], c=y, cmap='tab10')
+    # plt.show()
+
+    # 3-d plot
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x_low[:,0], x_low[:,1], x_low[:,2] , c=y[:10000], s=10) # select 10000 samples to plot
+    plt.show()
